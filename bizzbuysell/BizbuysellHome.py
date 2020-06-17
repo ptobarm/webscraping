f06b33d7da053ab90f8811e77b66f3747e1edcd3
Pablo Tobar <pablotobar@Pablos-MacBook-Pro.local>
2020-06-17 19:40:33 -0400

    Listado de Estados y url's en array

---
+++ bizzbuysell/BizbuysellHome.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium.webdriver.support.ui import Select
+
+
+driver = webdriver.Chrome()
+driver.get("https://www.bizbuysell.com/")
+#page = BeautifulSoup(driver.page_source,"html.parser")
+select = Select(driver.find_element_by_id('ddlStates'))
+options = select.options
+
+for option in options:
+    print(option.text)
+
+driver.close()
